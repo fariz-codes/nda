970dec798fffa857112b1f2a5d6768be3973adb7
mohammed fariz <mohmd.fariz@gmail.com>
2022-11-06 00:39:16 +0530

    Add enable-auth command: - Add option in command handler to add enable auth cmd - Add function to detect if command is ran with admin access

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 #!/usr/bin/env node
 
+const os = require('os');
+const isLinux = os.type().indexOf('Windows') > -1 ? false : true;
 const packageJson = require('./package.json');
-const commands = ['run', 'sleep', 'status', 'respawn'];
+const commands = ['run', 'sleep', 'status', 'respawn', 'enable-auth'];
 const optionsLength = {
   run: 4,
   sleep: 3,
   status: 3,
-  respawn: 3
+  respawn: 3,
+  'enable-auth': 5
 };
 const optionType = {
   run: 'number'
 };
+const adminAccessMsg = isLinux ? 'as a sudo user' : 'with admin privilege';
 
 const getArg = (index) => {
   return process.argv[index] ? process.argv.slice(index)[0].toString().trim() : '';
@@ -48,12 +52,14 @@ if (isValidCommand() && isValidOption()) {
   require('./lib/helpers/process-command');
 } else {
   console.log('Usage: nda <command> [option]\n');
-  console.log('nda run                         starts nda in default port 8055\n');
-  console.log('nda run [port]                  starts nda in mentioned port\n');
-  console.log('nda sleep                       stops nda & all the services started by it\n');
-  console.log('nda status                      provides the current running status of nda\n');
-  console.log('nda respawn                     restarts the nda & all the services started by it. If nda is already running in your machine, then this command must be used after installing a new version of nda to apply the changes in it.\n\n');
+  console.log(`nda enable-auth [username] [pwd] enables authentication. should be ran from a terminal ${adminAccessMsg}\n`);
+  console.log('nda run                          starts nda in default port 8055\n');
+  console.log('nda run [port]                   starts nda in mentioned port\n');
+  console.log('nda sleep                        stops nda & all the services started by it\n');
+  console.log('nda status                       provides the current running status of nda\n');
+  console.log('nda respawn                      restarts the nda & all the services started by it. If nda is already running in your machine, then this command must be used after installing a new version of nda to apply the changes in it.\n\n');
   console.log('Examples: \n');
-  console.log('nda run 7000                    starts nda in 7000 port\n');
+  console.log('nda enable-auth walter w@1Ter    enables authentication using the provided credentials\n');
+  console.log('nda run 7000                     starts nda in 7000 port\n');
 }
 
