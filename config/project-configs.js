7af966e9590876a07a0a34820872681c7e428e16
mohammed fariz <mohmd.fariz@gmail.com>
2025-04-13 23:37:24 +0530

    Add steps to protect qr code

--- config/project-configs.js
+++ config/project-configs.js
@@ -10,12 +10,13 @@ const homeDir = require('os').homedir();
 const authUserName = 'Admin';
 const authAppName = 'NDA';
 const authSecret = 'adm1nNd@Usr';
+const authQrCodeFile = 'nda_auth_otp';
 
 module.exports = {
   NDA_BASE_PATH: path.resolve(homeDir, '.nda'),
   PROJECTS_CONFIG_PATH: path.resolve(homeDir, '.nda/config/projects.json'),
   CHILD_PROCESS_BASE_CONFIG_PATH: path.resolve(homeDir, '.nda/config'),
   CHILD_PROCESS_LOG_PATH: path.resolve(homeDir, '.nda/logs'),
-  AUTH_QR_CODE_IMG_PATH: path.resolve(homeDir, '.nda/config/nda_auth_otp.png'),
-  authAppName, authUserName, authSecret
+  AUTH_QR_CODE_IMG_PATH: path.resolve(homeDir, '.nda/config/'),
+  authAppName, authUserName, authSecret, authQrCodeFile
 };
